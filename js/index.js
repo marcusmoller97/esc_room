62f3df102b44eaa674bd290d809e6420af4d4eea
marcusmoller97 <marcussmoller@icloud.com>
2024-11-08 14:22:20 +0100

    Removed styling inside index.js and made style inside scss document

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ div.addEventListener("click", createNav);
 
 function createNav () {
     const navContainer = document.createElement("nav");
+    navContainer.classList.add("mobile-nav");
     navContainer.id = "mobile-nav";
 
     /* create copy of header nav */
@@ -27,53 +28,27 @@ function createNav () {
     });
 
     /* add style to nav */
-    navContainer.style.position = "absolute";
-    navContainer.style.top = "0";
-    navContainer.style.bottom = "0";
-    navContainer.style.left = "0";
-    navContainer.style.right = "0";
     navContainer.style.opacity = "0";
-
-    navContainer.style.margin = "auto";
-
-    navContainer.style.width = "90%";
-    navContainer.style.height = "95%";
-    navContainer.style.backgroundColor = "#011827";
 
     /* add navContainer  */
     document.body.append(navContainer);
 
-    /* add style to list inside navContainer */
-    clonedLi.style.textAlign = "center";
-    clonedLi.style.marginTop = "auto";
-    clonedLi.style.marginBottom = "auto";
-    clonedLi.style.textDecoration = "none";
-    clonedLi.style.height = "calc(100% - 90px - 8rem";
-    clonedLi.style.display = "flex";
-    clonedLi.style.flexDirection = "column";
-    clonedLi.style.justifyContent = "center";
-    clonedLi.style.gap = "3rem";
-
     const listChildren = Array.from(clonedLi.children);
+    let count = 1;
     listChildren.forEach(ele => {
-        ele.style.listStyleType = "none";
-        ele.firstChild.style.color = "#FFF";
-        ele.firstChild.style.textDecoration = "none";
-        ele.firstChild.style.fontSize = "36px";
-        ele.firstChild.style.fontWeight = "bold";
+        ele.classList.add(`ele${count}`);
+        ++count;
     });
 
     /* create div for exitImg and add exitImg */
     const exitFig = document.createElement("article");
-    exitFig.style.height = "90px";
     exitFig.classList.add("exit-article");
+
     const exitImg = document.createElement("img");
     exitImg.src = "./img/close.png";
     exitImg.alt = "";
     exitImg.id = "exit-img";
 
-    exitImg.style.float = "right";
-    exitImg.style.padding = "1.5rem 1.5rem";
     exitImg.style.opacity = "0";
 
     exitFig.append(exitImg);
@@ -143,15 +118,8 @@ function hamburgerMenu () {
     const sectionLogo = document.querySelector(".section-flex");
 
     const div = document.createElement("div");
-
     div.classList.add("icon");
-    div.style.display = "flex";
-    div.style.float = "left";
-    div.style.flexDirection = "column";
-    div.style.alignSelf = "flex-start";
-    div.style.gap = "8px";
-    div.style.marginLeft = "auto";
-    div.style.marginRight = "20px";
+
     if (window.innerWidth > 965) {
         div.style.visibility = "hidden";
     }
